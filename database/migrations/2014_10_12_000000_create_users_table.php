53916f65544611bd24f0b7ef4d562ac0e217c77b
Emiliano Romero <enromero1994@gmail.com>
2021-12-27 22:48:11 -0300

    Cambio de formato del campo nacimiento de la tabla users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('lastname');
             $table->string('dni')->unique();
             $table->string('edad');
-            $table->date('nacimiento');
+            $table->dateTime('nacimiento');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
